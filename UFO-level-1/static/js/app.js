61788a5ff236cb293b1f4e66bd5e4cc2a62cc41d
Michael Regpala <mreg96@yahoo.com>
2021-04-02 13:12:33 -0700

    UFO 1 PUsh with updated bootstrap.css

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
+tableData.forEach(element => {
+    console.log(`{element.city}`) 
+});
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+
+tableData.forEach((siting) => {
+    var row = tbody.append("tr");
+    Object.entries(siting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
