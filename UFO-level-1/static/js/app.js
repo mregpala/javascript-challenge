a4e56cc28f14aec3c189a5a656187c23a303d75b
Michael Regpala <mreg96@yahoo.com>
2021-04-03 01:17:45 -0700

    Final UFO1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,13 +6,40 @@ tableData.forEach(element => {
 });
 // YOUR CODE HERE!
 
+
 var tbody = d3.select("tbody");
 
+var input = d3.select("#dateInput")
+var button = d3.select("#button")
 
-tableData.forEach((siting) => {
-    var row = tbody.append("tr");
+var inputValue = ""
+var tableDataMap = []
+function dataFilter(data) {
+  return (data.datetime == inputValue)
+}
+
+function handleChange(event){
+  inputValue = input.property("value");
+  tableData = data;
+  tableDataMap = tableData.filter(dataFilter);
+  tableData = tableDataMap.map(e => {return e;})
+  pop_table(tableData)
+}
+
+var row = ""
+var cell = ""
+function pop_table(data){
+tbody.selectAll("tr").remove();
+tbody.selectAll("td").remove();
+data.forEach((siting) => {
+    row = tbody.append("tr");
     Object.entries(siting).forEach(([key, value]) => {
-      var cell = row.append("td");
+      cell = row.append("td");
       cell.text(value);
+      console.log(value)
     });
-  });
+  })};
+
+pop_table(tableData);
+
+button.on("click", handleChange);
